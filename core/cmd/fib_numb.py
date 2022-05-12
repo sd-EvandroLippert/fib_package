dfe371cb91d1b48bb0b94b49bc8391ebe03ec076
Evandro Lippert Junior <evandro.lippert@gmail.com>
2022-05-12 14:39:27 -0300

    first commit

---
+++ core/cmd/fib_numb.py
@@ -0,0 +1,10 @@
+import argparse
+
+from core.fib_calcs.fib_number import recurring_fibonacci_number
+
+def fib_numb() -> None:
+    parser = argparse.ArgumentParser(description="Calculate Fibonacci numbers")
+    parser.add_argument("--number", action="store", type=int, required=True, help="Fibonacci number to be calculated")
+
+    args = parser.parse_args()
+    print(f"Your Fibonacci number is: {recurring_fibonacci_number(number=args.number)}")
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+ long_description = fh.read()
+
+setup(
+    name="flitton_fib_py",
+    version="0.0.1",
+    author="Evandro Lippert",
+    author_email="evandro.lippert@gmail.com",
+    description="Calculates a Fibonacci number",
+    url="https://github.com/EvandroLippert/fib_package",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    long_description=long_description,
+    classifiers=[
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+    
+    # To use the CLI script
+    entry_points={
+        'console_scripts': [
+        'fib-number = \
+        flitton_fib_py.cmd.fib_numb:fib_numb',
+        ],
+        },
+
+)
